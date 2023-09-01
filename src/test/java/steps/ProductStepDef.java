3451eb27eb3423818baa7686ed9f3dad48183afa
Adrian <johnadrian171@gmail.com>
2023-09-02 04:39:39 +0800

    updates in codes

--- src/test/java/steps/ProductStepDef.java
+++ src/test/java/steps/ProductStepDef.java
@@ -6,49 +6,49 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.HomePage;
 import pages.LoginPage;
-import pages.ProductPage;
 
 import java.util.List;
 
 public class ProductStepDef {
     Scenario scenario;
-    private ProductPage productpage = new ProductPage();
+    private HomePage homepage = new HomePage();
     private CommonUtil util = new CommonUtil();
 
 
     @When("^user sort products (.*)$")
     public void productSort(String DropdownItem) {
-        productpage.selectOnDropdown(DropdownItem);
-        util.takeScreenshot(productpage.getDriver(), productpage.getScenario());
+        homepage.selectOnDropdown(DropdownItem);
+        util.takeScreenshot(homepage.getDriver(), homepage.getScenario());
     }
 
     @Then("^user will see sorted products$")
     public void viewSortedProducts() {
-        productpage.getAllPrices();
+        homepage.getAllPrices();
     }
 
     @When("^user add multiple products$")
     public void addProducts(DataTable productNames) {
         List<List<String>> rows = productNames.asLists(String.class);
         for (List<String> column : rows) {
-            productpage.addProductToCart(column.get(0));
-            productpage.addProductToCart(column.get(1));
-            productpage.addProductToCart(column.get(2));
+            homepage.addProductToCart(column.get(0));
+            homepage.addProductToCart(column.get(1));
+            homepage.addProductToCart(column.get(2));
         }
-        util.takeScreenshot(productpage.getDriver(), productpage.getScenario());
+        util.takeScreenshot(homepage.getDriver(), homepage.getScenario());
     }
 
     @Then("^user should add multiple products to the cart$")
     public void validateCartCount() {
         int expectedCartCount = 3;
-        productpage.validateCartCount(expectedCartCount);
-        util.takeScreenshot(productpage.getDriver(), productpage.getScenario());
+        homepage.validateCartCount(expectedCartCount);
+        util.takeScreenshot(homepage.getDriver(), homepage.getScenario());
     }
 
     @Then("^user is navigated to Home Page$")
     public void userIsInHomePage() {
-        productpage.validateHomePage();
-        util.takeScreenshot(productpage.getDriver(), productpage.getScenario());
+        homepage.validateHomePage();
+        util.takeScreenshot(homepage.getDriver(), homepage.getScenario());
     }
 }
